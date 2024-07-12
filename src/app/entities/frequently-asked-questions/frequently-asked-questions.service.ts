3b2de3b42468d082dafd3cb522aefc429cd5deb9
derfex <derfex@yandex.ru>
2024-07-12 22:28:42 +0300

    #8. Feature (frequently-asked-questions): Create the service

---
+++ src/app/entities/frequently-asked-questions/frequently-asked-questions.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { FrequentlyAskedQuestion } from '~entities/frequently-asked-questions/frequently-asked-questions.type'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FrequentlyAskedQuestionsService {
+  constructor(
+    private readonly httpClient: HttpClient,
+  ) {}
+
+  public getFrequentlyAskedQuestions(): Observable<ReadonlyArray<FrequentlyAskedQuestion>> {
+    return this.httpClient.get<ReadonlyArray<FrequentlyAskedQuestion>>(frequentlyAskedQuestionsURL)
+  }
+}
+
+const frequentlyAskedQuestionsURL = 'https://univt.github.io/student-data/data/entities/frequently-asked-questions/frequently-asked-questions.json'
