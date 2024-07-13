de82bf4190d83cfecd7f0aa83dbe8ebe5fc53edb
derfex <derfex@yandex.ru>
2024-07-13 03:18:51 +0300

    #8. Performance (price-list): Use `takeUntilDestroyed`

--- src/app/entities/ut-services/price-list/price-list.component.ts
+++ src/app/entities/ut-services/price-list/price-list.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { CardModule } from 'primeng/card'
+import { UTServicesService } from '~entities/ut-services/ut-services.service'
 import { UTService } from '~entities/ut-services/ut-services.type'
-import { UTServicesService } from '../ut-services.service'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,14 +15,16 @@ import { UTServicesService } from '../ut-services.service'
 export class PriceListComponent implements OnInit {
   public services: ReadonlyArray<UTService> = []
 
+  private readonly destroyRef = inject(DestroyRef)
+
   constructor(
     private readonly cdr: ChangeDetectorRef,
     private readonly uTServicesService: UTServicesService,
-    
   ) {}
 
   public ngOnInit(): void {
     this.uTServicesService.getUTServices()
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((services: ReadonlyArray<UTService>): void => {
         this.services = services
         this.cdr.markForCheck()
