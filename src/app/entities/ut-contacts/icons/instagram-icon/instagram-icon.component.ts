2862494f815b3a8c5f84c2d320e026ab692cf920
derfex <derfex@yandex.ru>
2024-07-19 04:40:10 +0300

    #8. Feature (ut-contacts/icons): Create the components

---
+++ src/app/entities/ut-contacts/icons/instagram-icon/instagram-icon.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  selector: 'app-instagram-icon',
+  standalone: true,
+  styleUrl: './instagram-icon.component.sass',
+  templateUrl: './instagram-icon.component.html',
+})
+export class InstagramIconComponent {}
